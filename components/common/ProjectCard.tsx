bbc3ba3f4cff74dc9045efcbb68e66100edc9a74
Davdas <davide_mancini82@yahoo.it>
2022-11-17 10:31:51 +0100

    scroll fixed

--- components/common/ProjectCard.tsx
+++ components/common/ProjectCard.tsx
@@ -21,28 +21,24 @@ const ProjectCard = ({
   linkUrl,
 }: ProjectCardProps) => {
   return (
-    <div className="cursor-pointer w-full h-full relative sm:hover:scale-[101%] ease-in duration-200 p-4 rounded-md shadow-xl shadow-black bg-zinc-800 mb-4">
-      <div
-        className={`w-full min-h-[300px] bg-cover bg-center `}
-        style={{ backgroundImage: `url(/${imgUrl})` }}
-      />
-      <Link href={linkUrl}>
-        <p className=" text-white px-4 mt-3 underline underline-offset-8 hover:text-gray-400 inline-block">
-          Vedi Foto
-        </p>
-      </Link>
-
-      <div className=" p-4 mt-5">
-        <h2 className=" text-amber-600 uppercase tracking-wide text-3xl mt-3">
-          {title}
-        </h2>
-        <div className=" mt-5">
-          <p className=" text-white italic mb-2">{text}</p>
-          <p className=" text-white italic mb-2">{text1}</p>
-          <p className=" text-white italic mb-2">{text2}</p>
+    <Link href={linkUrl}>
+      <div className="cursor-pointer w-full h-full relative sm:hover:scale-[98%] ease-in duration-100 hover:bg-zinc-900 p-4 rounded-md shadow-xl shadow-black bg-zinc-800 mb-4">
+        <div
+          className={`w-full min-h-[300px] bg-cover bg-center `}
+          style={{ backgroundImage: `url(/${imgUrl})` }}
+        />
+        <div className=" p-4 mt-5">
+          <h2 className=" text-amber-600 uppercase tracking-wide text-3xl mt-3">
+            {title}
+          </h2>
+          <div className=" mt-5">
+            <p className=" text-white italic mb-2">{text}</p>
+            <p className=" text-white italic mb-2">{text1}</p>
+            <p className=" text-white italic mb-2">{text2}</p>
+          </div>
         </div>
       </div>
-    </div>
+    </Link>
   );
 };
 
--- pages/index.tsx
+++ pages/index.tsx
@@ -15,6 +15,10 @@ const Home: NextPage = () => {
         ></meta>
         <meta name="author" content="Davide Mancini"></meta>
         <meta name="description" content="artigiani del legno" />
+        <meta
+          name="viewport"
+          content="width=device-width, initial-scale=1.0"
+        ></meta>
         <title>Falegnameria Mancini</title>
       </Head>
 
