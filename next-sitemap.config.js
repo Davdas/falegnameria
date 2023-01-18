cd055933e7461e48bbca0f4952b98cd480b789a0
Davdas <davide_mancini82@yahoo.it>
2023-01-18 22:10:32 +0100

    aggiunto file verifica ludo

--- next-sitemap.config.js
+++ next-sitemap.config.js
@@ -1,6 +1,6 @@
 /** @type {import('next-sitemap').IConfig} */
 module.exports = {
-  siteUrl: "https://mancinifalegnameria.it",
+  siteUrl: "https://www.mancinifalegnameria.it",
   generateRobotsTxt: true,
   outDir: "/layuot/footer.tsx",
 };
